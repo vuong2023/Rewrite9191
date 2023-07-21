9a2d13a3185e27e2c2c015bf24085e1b1398af40
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-22 05:51:02 +0800

    Create ypm.js

---
+++ ypm.js
@@ -0,0 +1,18 @@
+var Baby = {
+  "adsDisabledTill": 101966821994000,
+  "displayName": "Baby",
+  "loginId": "cell:18888888888",
+  "email": "8888***@qq.com",
+  "gender": "1",
+  "level": 1,
+  "avatar": "https://raw.githubusercontent.com/Yu9191/-/main/nv4.png",
+  "isMember": true,
+  "ladder": "member",
+  "dailyPlayAwardCoupon": null,
+  "score": 66666666,
+  "hasWX": true,
+  "hasOnlyWeibo": false,
+  "creationTime": 5
+};
+
+$done({ body: JSON.stringify(Baby) });
