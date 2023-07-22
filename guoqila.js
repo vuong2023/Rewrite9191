92165919b1d41fea67b288d809144998267cbc87
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-22 14:22:09 +0800

    Update guoqila.js

--- guoqila.js
+++ guoqila.js
@@ -15,23 +15,43 @@
 hostname = api.guoqi365.com
 
 */
-var responseData = {
+let responseData = {
   "result": {
     "data": {
       "user_is_pro": true,
       "group_is_pro": true,
-      "membership_type": 5,
+      "membership_type": 3,
       "event": {
         "valid": true,
         "id": "636a0b84f70b896dbde88aa6",
         "icon": "https://raw.githubusercontent.com/Yu9191/-/main/nv5.PNG",
         "type": "member",
         "name": "baby",
-        "desc": "Baby wish you happy every day"
-      }
+        "desc": "baby love you"
+      },
+      "locked": false,
+      "expires_date": "1888888888888888"
     },
     "code": 1
   }
 };
 
-$done({ body: JSON.stringify(responseData) });
+let i = JSON.parse($response.body);
+
+if (i && i.result && i.result.data) {
+  i.result.data.user_is_pro = true;
+  i.result.data.group_is_pro = true;
+  i.result.data.membership_type = 2;
+  i.result.data.locked = false;
+  i.result.data.name = "baby";
+  i.result.data.desc = "baby love you";
+  i.result.data.expires_date = "1888888888888888";
+
+  if (i.result.data.event && !i.result.data.event.icon.startsWith("https://raw.githubusercontent.com/Yu9191/-/main/nv5.PNG")) {
+    i.result.data.event.icon = "https://raw.githubusercontent.com/Yu9191/-/main/nv5.PNG";
+  }
+
+  i.app && (i.app = []);
+}
+
+$done({ body: JSON.stringify(i) });
