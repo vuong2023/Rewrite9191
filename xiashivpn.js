c92200af07a3924571008184348c4b9a19b7c007
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-26 01:20:12 +0800

    Update xiashivpn.js

--- xiashivpn.js
+++ xiashivpn.js
@@ -4,7 +4,7 @@ name：夏时国际VPN
 to：https://t.cn/A60vysg2
 me：@ios151 Thank@zhangpeifu
 Ts：Borrowing @zhangpeifu's script
-tp:host为“*” 可能和其他软件冲突 用完记得关
+
 
 [rewrite_local]
 ^https?:\/\/(\w+\.com|198\.18\.76\.\d+)\/addressx5\/* url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/xiashivpn.js
