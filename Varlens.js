2c555422ff70ff69cd55e28d5554f174090ccf36
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-28 11:58:42 +0800

    Create Varlens.js

---
+++ Varlens.js
@@ -0,0 +1,13 @@
+/*
+
+项目名称：Varlens解锁Pro滤镜相机等
+下载地址：还没填
+脚本作者：@ios151
+使用声明：本人付费共享token,仅供参考，禁止转载与售卖！
+
+
+[rewrite_local]
+^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/v3\/ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Varlens.js
+
+[mitm]
+hostname = mid.zineapi.com
