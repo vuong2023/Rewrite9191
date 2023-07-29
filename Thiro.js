b596933c1d1074ff9b46abec65bc2af94052886f
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-30 01:21:20 +0800

    Create Thiro.js

---
+++ Thiro.js
@@ -0,0 +1,29 @@
+/*
+
+项目名称：Thiro
+下载地址：https://apps.apple.com/cn/app/thiro/id1555982483
+脚本作者：ios151
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+
+[rewrite_local]
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(.*?)*$) url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Thiro.js
+
+*/
+
+var lovebaby = JSON.parse($response.body);
+
+lovebaby.subscriber.subscriptions["atelerix_pro_lifetime"] = {
+  "purchase_date": "2022-09-09T09:09:09Z",
+  "original_purchase_date": "2022-09-09T09:09:09Z",
+  "ownership_type": "PURCHASED"
+};
+
+lovebaby.subscriber.entitlements["pro"] = {
+  "ownership_type": "PURCHASED",
+  "product_identifier": "atelerix_pro_lifetime",
+  "original_purchase_date": "2022-09-09T09:09:09Z",
+  "purchase_date": "2022-09-09T09:09:09Z"
+};
+
+$done({ body: JSON.stringify(lovebaby) });
