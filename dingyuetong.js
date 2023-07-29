74f79a1e777049f4cefb538011a42989a801a333
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-29 16:07:30 +0800

    更新 dingyuetong.js

--- dingyuetong.js
+++ dingyuetong.js
@@ -8,7 +8,7 @@
 
 
 [rewrite_local]
- url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/dingyuetong.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/dingyuetong.js
 
 
 [mitm]
