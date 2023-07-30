2faaa8775eecd6d5e782e5995178a1ba6809f027
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-30 18:33:05 +0800

    更新 videoday.js

--- videoday.js
+++ videoday.js
@@ -5,7 +5,7 @@
 
 [rewrite_local]
 
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/iScreenk.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Yu9191/main/videodayjs
 
 
 [mitm]
