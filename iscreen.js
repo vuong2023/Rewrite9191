acaf891eba93396e08c485aed05810ac1df44c05
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-07-31 11:38:59 +0800

    更新 iscreen.js

--- iscreen.js
+++ iscreen.js
@@ -14,16 +14,30 @@
 hostname = cs.kuso.xyz
 
 */
+var ios151 = JSON.parse($response.body);
+ios151.data.FeedBanner = "3333";
+ios151.data.IsandBannerAd = 0;
+ios151.data.noAds = 1;
+ios151.data.lang = "zh";
+ios151.data.review_pop_interval = 300;
+ios151.data.country = "cn";
+ios151.data.lockscreen_noVip = "0,1";
+ios151.data.SplashInter = 300;
+ios151.data.pic_lock_count = 50;
+ios151.data.showOurApp = 1;
+ios151.data.main_pop_ad_interval = 0;
+ios151.data.launchAd = 0;
+ios151.data.FeedAd = 0;
+ios151.data.BannerAd = 0;
+ios151.data.vipPopupType = 3;
+ios151.data.ai_painting = 1;
+ios151.data.ai_painting_wp = 1;
+ios151.data.activityUpdateTime = 1687959242;
+ios151.data.rewardAd = 0;
+ios151.data.Hudong_Interval = 900;
+ios151.data.ThemeFeedAd = 4;
+ios151.data.showRing = 1;
+var modifiedBody = JSON.stringify(ios151);
+$done({ body: modifiedBody });
 
-
-var body=$response.body;
-
-body = body.replace(/"FeedAd":\d+/g,'"FeedAd":2');
-body = body.replace(/"ab":\d+/g,'"ab":1');
-body = body.replace(/"noAds":\d+/g,'"noAds":1');
-body = body.replace(/"rewardAd":\d+/g,'"rewardAd":0');
-body = body.replace(/"mainPop_open":\d+/g,'"mainPop_open":1');
-body = body.replace(/"vipPopupType":\d/g,'"vipPopupType":2');
-
-$done(body);
 
