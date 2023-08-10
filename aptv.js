7d59b41890245d183ffd68dcb6f0a8d87f348ae2
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-11 00:25:12 +0800

    更新 aptv.js

--- aptv.js
+++ aptv.js
@@ -17,8 +17,43 @@ hostname = api.revenuecat.com
 
 */
 
+var objc = JSON.parse($response.body);
 
+    objc = {
+  "request_date_ms": 1691684430913,
+  "request_date": "2023-08-10T16:20:30Z",
+  "subscriber": {
+    "non_subscriptions": {
+    },
+    "first_seen": "2023-08-10T16:15:27Z",
+    "original_application_version": "1",
+    "other_purchases": {
+    },
+    "management_url": null,
+    "subscriptions": {
+      "com.kimen.aptvpro.lifetime": {
+        "store": "app_store",
+        "purchase_date": "2023-02-23T02:33:33Z",
+        "ownership_type": "PURCHASED",
+        "original_purchase_date": "2023-02-23T02:33:33Z",
+        "expires_date": "6666-06-06T06:06:06Z"
+      }
+    },
+    "entitlements": {
+      "pro": {
+        "store": "app_store",
+        "purchase_date": "2023-02-23T02:33:33Z",
+        "ownership_type": "PURCHASED",
+        "original_purchase_date": "2023-02-23T02:33:33Z",
+        "product_identifier": "com.kimen.aptvpro.lifetime",
+        "expires_date": "6666-06-06T06:06:06Z"
+      }
+    },
+    "original_purchase_date": "2022-10-02T04:32:28Z",
+    "original_app_user_id": "$RCAnonymousID:807d56e7d09e4e1f8cfa7e50cd323c6b",
+    "last_seen": "2023-08-10T16:15:27Z"
+  }
+}
 
+$done({body : JSON.stringify(objc)});
 
-
-var version_='jsjiami.com.v7';const r=b;(function(c,d,e,f,g,h,i){return c=c>>0x7,h='hs',i='hs',function(j,k,l,m,n){const q=b;m='tfi',h=m+h,n='up',i+=n,h=l(h),i=l(i),l=0x0;const o=j();while(!![]&&--f+k){try{m=-parseInt(q(0x9b,'$M*u'))/0x1*(-parseInt(q(0x9d,'LpD5'))/0x2)+parseInt(q(0xad,')ySY'))/0x3*(-parseInt(q(0xab,'Lp$*'))/0x4)+-parseInt(q(0xb0,'Zo!F'))/0x5+parseInt(q(0xa3,'wUuc'))/0x6*(parseInt(q(0x9f,'p@Qi'))/0x7)+-parseInt(q(0xa6,'$M*u'))/0x8*(-parseInt(q(0xb8,'wUuc'))/0x9)+-parseInt(q(0xb5,'LpD5'))/0xa*(parseInt(q(0x9e,'zU0l'))/0xb)+parseInt(q(0xa2,'wNgT'))/0xc*(parseInt(q(0x9a,'X]!]'))/0xd);}catch(p){m=l;}finally{n=o[h]();if(c<=f)l?g?m=n:g=n:l=n;else{if(l==g['replace'](/[HIJAFQklLyfthSEDKUWnBR=]/g,'')){if(m===k){o['un'+h](n);break;}o[i](n);}}}}}(e,d,function(j,k,l,m,n,o,p){return k='\x73\x70\x6c\x69\x74',j=arguments[0x0],j=j[k](''),l='\x72\x65\x76\x65\x72\x73\x65',j=j[l]('\x76'),m='\x6a\x6f\x69\x6e',(0x135e12,j[m](''));});}(0x6300,0xabb7b,a,0xc8),a)&&(version_=a);function b(c,d){const e=a();return b=function(f,g){f=f-0x9a;let h=e[f];if(b['StogGQ']===undefined){var i=function(n){const o='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';let p='',q='';for(let r=0x0,s,t,u=0x0;t=n['charAt'](u++);~t&&(s=r%0x4?s*0x40+t:t,r++%0x4)?p+=String['fromCharCode'](0xff&s>>(-0x2*r&0x6)):0x0){t=o['indexOf'](t);}for(let v=0x0,w=p['length'];v<w;v++){q+='%'+('00'+p['charCodeAt'](v)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(q);};const m=function(n,o){let p=[],q=0x0,r,t='';n=i(n);let u;for(u=0x0;u<0x100;u++){p[u]=u;}for(u=0x0;u<0x100;u++){q=(q+p[u]+o['charCodeAt'](u%o['length']))%0x100,r=p[u],p[u]=p[q],p[q]=r;}u=0x0,q=0x0;for(let v=0x0;v<n['length'];v++){u=(u+0x1)%0x100,q=(q+p[u])%0x100,r=p[u],p[u]=p[q],p[q]=r,t+=String['fromCharCode'](n['charCodeAt'](v)^p[(p[u]+p[q])%0x100]);}return t;};b['rbTroP']=m,c=arguments,b['StogGQ']=!![];}const j=e[0x0],k=f+j,l=c[k];return!l?(b['CWZkUX']===undefined&&(b['CWZkUX']=!![]),h=b['rbTroP'](h,g),c[k]=h):h=l,h;},b(c,d);}function a(){const s=(function(){return[version_,'fkKjASysLljiWaEmJShiD.ScQoUymFn.HvL7BItR==','WQCcW5zxW7ZcKGjd','W4VdPKldJCk3W7dcPIFdHmohWOvXW4nGuYVdSLdcV8kn','W4xdPulcLCowWQVdNLVcPq','WQfyBgG','gW/cKCo5BSk5WQ/dOu3dHSk/uW','W7Okys8+WQZdR8oXWONcR8onW7e','uh8iW6bDqXtcQmoa','Fmk0qvKJW7iEW4CPc28YW7u','aM/dMcPFWQFdTCk8hxiuWRK','umkHWOW2WPqDW48','FXruWPBcVspdQvHLva','wa7dUHJdISoJvCo/'].concat((function(){return['W4LJW7WFWQ/dHmoOWOpcTamxWPS5','W5acWP7cUSkxWQfMW7LT','W4HpamkIWRRcK8kFC8kkuSom','u1pdMhxdHZGOqvNdJgSxCG','WPPzbrldT3rRWP5WWOtdSeZdHW','uCkDCCoYy2tcVvb7cSoYW5xcHW','uColW65XW5ToWPFdPddcSmknWQ13eSkhWR7dVtZcVv0','WP7dLmojWOFcLSoZoa3cItG','WRqAzSkwsCoqALfvvZC','W7z1W55ZwcVcRmomDxFcHbu+tmksWRewWQBcH8o3','W4vEi33cHSkgcWDVW7fAn8kLECofDuCAW6zA','zea/cmoUW4WitmkVkmkkW4nrWO5GsSk+W4q1jLtdQ1/cVCk5xG','W79HqSolW4OIWQJdI8ou','WQtdUI4OW4vPfSoJDW'].concat((function(){return['qSkrx2VcNa','dmk0W57cI0DxWOSdsa','W4tdPeldJSkOWOJdPNZcTSkzWR0','n13dLSkJlZ3cRhpcU8kZfsLMyCoOnspdHarX','uSogamkfadRdGG','W6OLW7HviqZdTmoa','WQVdHComWPdcJf7dSY8','crVdMSoTd3HhW57cKmo4WPVdNCok','xLddNNBdHejRFNtdJ0u','m8kaxh7dV8kKfc01AxddVG','WRKCy8kEqSk+q31xFqJcUW'];}()));}()));}());a=function(){return s;};return a();};const Istolethescript=JSON[r(0xbe,'92h6')](typeof $response!==r(0xb1,'vtdR')?$response[r(0xa7,'l8$G')]:null);Istolethescript&&Istolethescript['subscriber']?(Istolethescript[r(0xae,'eRxS')][r(0xb4,'7Y!p')]={'com.kimen.aptvpro.lifetime':{'expires_date':r(0xa5,'$M*u'),'original_purchase_date':r(0xb9,'OCip'),'purchase_date':'2023-02-23T02:33:33Z','ownership_type':'PURCHASED','store':'app_store'}},Istolethescript[r(0xb7,'o31q')][r(0xa8,'x7WQ')]={'pro':{'expires_date':r(0xba,'Ryn7'),'original_purchase_date':r(0x9c,'RbSs'),'purchase_date':r(0xb6,')ySY'),'ownership_type':'PURCHASED','product_identifier':r(0xbb,'gwZj'),'store':r(0xbd,'hyLD')}},$done({'body':JSON['stringify'](Istolethescript)})):$done({});var version_ = 'jsjiami.com.v7';
