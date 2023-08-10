c86d6dce50cdcb318b2263bd21efb0a9e40d854d
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2023-08-10 21:30:30 +0700

    Update xTerminal.js

--- xTerminal.js
+++ xTerminal.js
@@ -7,7 +7,7 @@
 
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/‎xTerminal.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/vuong2023/Rewrite9191/main/xTerminal.js
 
 
 [mitm]
