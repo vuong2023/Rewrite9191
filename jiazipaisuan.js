47f04e43defcb6a460b1b5afc3ce2772a9a34518
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-20 22:43:00 +0800

    Update jiazipaisuan.js

--- jiazipaisuan.js
+++ jiazipaisuan.js
@@ -10,7 +10,7 @@
 
 [rewrite_local]
 
-https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body APTV.js
+https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/jiazipaisuan.js
 
 [mitm] 
 
